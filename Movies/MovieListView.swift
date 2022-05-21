6d45baf655dc6be5fd92f3781094cba7bce00a39
Omar Amoako <omar.amoako@yahoo.co.uk>
2022-05-21 14:40:33 +0100

    Initial Commit

---
+++ Movies/MovieListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Omar on 21/05/2022.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Omar on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
